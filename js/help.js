07062fa3260f402d3267ed411d80d4379fb66acb
Jessey Cool <jd.jessey@gmail.com>
2014-07-24 19:21:28 +0200

    1.6.1
    -add BigScreen brad.is/coding/BigScreen/
    -add Fullscreen mode for canvas
    -fix removed to many spaces in index.htm

--- js/help.js
+++ js/help.js
@@ -7,4 +7,28 @@ $(document).ready(function() {
 	});
 
 });
+//fullscreen function
+function fullScreen() {
+    if (BigScreen.enabled) {
+        BigScreen.request(canvas, onEnter, onExit, onError); //var canvas is form js/script.js
+        // You could also use .toggle(element, onEnter, onExit, onError)
+    }
+    else {
+        // fallback for browsers that don't support full screen
+		say("Your browser don`t support full screen. Maybe you can try it in another browser or play normal size.");
+    }
+}
+onEnter = function() {
+    // called when the first element enters full screen
+}
+onError = function() {
+    // called when there is an error
+	say("There is an ERROR with entering Fullscreen mode. Maybe you can try it in another browser or play normal size.");
+}
+onExit = function() {
+    // called when all elements have exited full screen
+}
+function say(str) {
+	alert("Oeps, "+str+"	;(");
+}
 
